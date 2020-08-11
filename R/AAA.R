5d497a54c85955dea1fba77bd4d1b299f9d57623
Leonardo Ramirez-Lopez <ramirez.lopez.leo@gmail.com>
2020-08-11 09:32:05 +0200

    version 2.1

--- R/AAA.R
+++ R/AAA.R
@@ -3,7 +3,7 @@
 .onAttach <- function(lib, pkg) {
   # assign("gpclib", FALSE, envir=.RESEMBLE_CACHE)
   resemble_v <- read.dcf(file = system.file("DESCRIPTION", package = pkg), fields = "Version")
-  packageStartupMessage(paste0("\033[34m", pkg, " version ", resemble_v, " -- 'gordillo'\033[39m"))
+  packageStartupMessage(paste0("\033[34m", pkg, " version ", resemble_v, " -- '????'\033[39m"))
   packageStartupMessage("\033[34mcheck the package repository at https://github.com/l-ramirez-lopez/resemble/\033[39m")
 }
 
